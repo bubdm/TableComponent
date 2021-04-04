37f4dc3fb3401809da2c6fa20905e88c075ec103
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2021-04-03 21:02:55 -0300

    Added media and example project
    Example Project

---
+++ Example/DbClickableTableComponent/DataAccess/DummyDataAccess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class DummyDataAccess
+    {
+        public static async Task<List<EmployeesDummyClass>> GetAllEmployeesAsync()
+        {
+            await Task.Delay(2000);
+            return new List<EmployeesDummyClass>
+            {
+                new EmployeesDummyClass
+                {
+                    Age = 15,
+                    Id = "1",
+                    DateOfBirth = DateTime.Now
+                },
+                new EmployeesDummyClass
+                {
+                    Age = 20,
+                    Id = "2",
+                    DateOfBirth = DateTime.Now
+                }
+};
+        }
+        public static async Task<List<UsersDummyClass>> GetAllUsersAsync()
+        {
+            await Task.Delay(2000);
+            return new List<UsersDummyClass>
+            {
+                new UsersDummyClass
+                {
+                    Age = 14,
+                    FirstName = "Leo",
+                    LastName = "Silva"
+                },
+                new UsersDummyClass
+                {
+                    Age = 10,
+                    FirstName = "Gabriel",
+                    LastName = "Huszcza"
+                }
+            };
+        }
+    }
+}
---
+++ Example/DbClickableTableComponent/DataAccess/EmployeesDummyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class EmployeesDummyClass
+    {
+        public string Id { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
